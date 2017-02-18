e12c34d78fc084131dc061dc09603f3c88029120
Felipe Corrêa da Silva Sanches <fsanches@metamaquina.com.br>
2017-02-18 16:50:37 -0200

    inicio do escaneamento

---
+++ PDF/make_pdf.sh
@@ -0,0 +1,12 @@
+echo "Generating lowres pages:"
+for f in ../originais/*.tiff
+do
+    echo "Converting $f ..."
+    convert $f -resize 1200 $f.lowres.jpg
+done
+
+echo "And now generating the lowres PDF..."
+convert ../originais/Capa.lowres.jpg ../originais/Pagina_*.lowres.jpg Software_para_o_Sintetizador__Guido_Stolfi.pdf
+rm ../originais/*.lowres.jpg -f
+
+echo "done."
